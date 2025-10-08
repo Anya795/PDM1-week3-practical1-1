ec50435d8c8d4fb86d643c3936fdd5a02dc74c37
Anya795 <psd540@york.ac.uk>
2025-10-08 12:56:18 +0100

    hi

---
+++ exercise2-1/sketch.js
@@ -0,0 +1,24 @@
+let circleX = 300
+let circleY = 300
+let circleW = 550
+
+
+function setup() {
+createCanvas(600,600);
+fillColour = color(random(255), random(255));
+fill(fillColour);
+circle(circleX, circleY, circlew);
+}
+
+function draw () {
+    background(255);
+    triangle(30, 75, 58, 20, 86, 75);
+    
+    function KeyPressed() {
+        if (key === 'w');{
+            circleY -= 5
+        }
+       
+    }
+    
+}
